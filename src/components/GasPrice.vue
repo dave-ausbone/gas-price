7f4c91ba905e146af1cce2ee7e79b7ebfc455a34
David Sebaoun <dsebaoun@payplug.com>
2021-10-27 17:43:58 +0200

    create wei options blocks and add auto-refresh of these values

--- src/components/GasPrice.vue
+++ src/components/GasPrice.vue
@@ -1,17 +1,37 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String
-})
-
-const count = ref(0)
+<script>
+export default {
+  name: 'GasPrice',
+  data() {
+    return {
+      average: 0,
+      safeLow: 0,
+      fast: 0,
+      fastest: 0,
+    }
+  },
+  methods: {
+    loadData() {
+      /*$.get('/api/data', function (response) {
+        // TODO: save data
+      }.bind(this));*/
+      console.log("loadData");
+      this.average += 0.1;
+      this.safeLow += 0.1;
+      this.fast += 0.1;
+      this.fastest += 0.1;
+    }
+  },
+  created() {
+    this.interval = setInterval(() => this.loadData(), 1000);
+  }
+}
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>Welcome</p>
+  <p>Average : {{average}}</p>
+  <p>Safe Low : {{safeLow}}</p>
+  <p>Fast : {{fast}}</p>
+  <p>Average : {{fastest}}</p>
 </template>
 
 <style scoped>
